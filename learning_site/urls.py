1884727d3c66c0a9afcf0daccc2c6a67743163bf
Mauricio Eduardo <olive.y.ra@hotmail.com>
2016-08-26 08:52:25 -0300

    add o css

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from . import views
 
@@ -24,3 +25,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.hello_word),
 ]
+
+urlpatterns += staticfiles_urlpatterns()

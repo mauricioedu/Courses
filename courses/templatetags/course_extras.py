487d8eadbb9e89ff589603c44d3f5839caac3db2
Mauricio Eduardo <olive.y.ra@hotmail.com>
2016-09-08 19:43:58 -0300

    Add layout e formulario

---
+++ courses/templatetags/course_extras.py
@@ -0,0 +1,31 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+import markdown2
+
+from courses.models import Course
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def newest_course():
+        return Course.objects.latest('created_at')
+
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+        courses = Course.objects.all()
+        return {'courses': courses}
+
+@register.filter('time_estimate')
+def time_estimate(word_count):
+        minutes = round(word_count/20)
+        return minutes
+
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+        html_body = markdown2.markdown(markdown_text)
+        return mark_safe(html_body)
--- learning_site/settings.py
+++ learning_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'courses',
 ]
 
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'suggestions') 
